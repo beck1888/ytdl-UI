8aeadd306b5ead962b855aeb8381c8d6949b220c
Beck Orion <beckorion@gmail.com>
2023-11-29 09:04:39 -0800

    Condensed code and added comments for clarity

--- main.py
+++ main.py
@@ -1,91 +1,65 @@
-# From ui_window.py
+# main.py
 
+# Import libraries
+import tkinter as tk # deals with gui
+from tkinter import ttk # same module as above, just called in a specific way
+import os # used to run the formatted yt-dl command in the users terminal
+
+# Define functions
 def show_ask_UI_window():
-    import tkinter as tk
-    from tkinter import ttk
-    # import os
-
     def on_submit():
         global selected_option
         global entered_text
         selected_option = dropdown.get()
         entered_text = entry.get()
-
-
-        # Close the pop-up window
         popup.destroy()
-
-        # Kill the Python process
-        root.destroy()
-
-        # return [selected_option, entered_text]
+        root.destroy() # Kill the Python app process (stop bouncing in the dock)
 
     # Create the main window
     root = tk.Tk()
     root.withdraw()
     root.title("YouTube-dl UI")
 
-    # Create a pop-up window
+    # Create the pop-up window
     popup = tk.Toplevel(root)
     popup.geometry("600x250")
 
-
-    # Dropdown options
-    options = ['audio (.mp3)', 'video (.mp4)']
+    options = ['audio (.mp3)', 'video (.mp4)'] # Dropdown options for the pop-up
 
     # Create and place the dropdown
     dropdown_label = ttk.Label(popup, text="Select a format for your download")
     dropdown_label.pack(pady=10)
-
     dropdown = ttk.Combobox(popup, values=options, state="readonly")
     dropdown.pack(pady=10)
 
-    # Create and place the text box with a wider width (e.g., width=30)
+    # Create and place the text box
     entry_label = ttk.Label(popup, text="Paste video the full URL:")
     entry_label.pack(pady=10)
-
-    entry = ttk.Entry(popup, width=50)  # Adjust the width as needed
+    entry = ttk.Entry(popup, width=50)
     entry.pack(pady=10)
 
     # Create and place the submit button
     submit_button = ttk.Button(popup, text="Start Download", command=on_submit)
     submit_button.pack(pady=20)
 
-    # Run the main loop
+    # Run the main loop (needed for tkinter to function)
     root.mainloop()
 
-def return_selections():
+def return_selections(): # Outputs selected format and pasted URL which becomes a list
     list_of_return = [selected_option, entered_text]
     return list_of_return
 
+# CODE THAT RUNS STARTS HERE
+show_ask_UI_window() # Puts up the graphical window that asks for the inputs
 
+received_list = return_selections() # Gets the selections and inputs from the GUI as a list
 
+url = received_list[1] # Set the URL to the second item in the list returned
 
-# - - - - - - 
-# Start of active code
-
-
-import os
-
-show_ask_UI_window()
-
-received_list = list(return_selections())
-
-# Clean up media type and set URL to own var
-if received_list[0] == 'audio (.mp3)':
-    media_type = 'mp3'
-else:
-    media_type = 'mp4'
-
-url = received_list[1]
-
-# print(f"{media_type}, {url}") # Check outputs
-
-# Build command
-# PLEASE KEEP IN MIND THAT THIS COMMAND ASSUMES THE .CONFIG FILE IS SET TO DESKTOP
-if media_type == 'mp3': # Command for audio only
+# Build the command to run imn the terminal
+if received_list[0] == 'audio (.mp3)': # If the user only wants audio
     command = f"youtube-dl --extract-audio --audio-format mp3 '{url}'"
-else: # Mp4 file
+else: # If audio is not selected, the other option (mp4) is what the user wants
     command = f"youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4' '{url}'"
 
 
